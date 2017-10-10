cbfe5b9cca45061da25f5fb4992585c891c5c535
Lambyor <lambyor@gmail.com>
2017-10-10 18:22:15 +0200

    1-step Verlet. Verry accurate.

--- main_and_affichage.py
+++ main_and_affichage.py
@@ -2,27 +2,36 @@ from truc import *
 import pygame
 import time
 
-
+COLORS = [(0, 0, 255), (0, 255, 0), (255, 255, 255)]
 pygame.init()
 screen = pygame.display.set_mode((750, 2050))
 done = False
+draw = True
 
 modele = Modele()
 i = 0
 points = []
 while not done:
+    if draw:
+        screen.fill((0, 0, 0))
+
     for event in pygame.event.get():  # On parcours la liste de tous les événements reçus
         if event.type == pygame.QUIT:  # Si un de ces événements est de type QUIT
             done = True
-    i += 1
-    if i%1000 is 0:
-        a = modele.routine(True)
-        for c in a:
-            c[0] = int(c[0]/3000000)
-            c[1] = int(c[1]/3000000)
-            pygame.draw.circle(screen, (0, 0, 255), c, 1, 1)
+        elif event.type == pygame.KEYDOWN:
+            draw = not draw
 
+    i += 1
+    if i%10 is 0:
+        a = modele.routine(True)
+        j = 0
+        for c in a:
+            c[0] = int(c[0]/1)
+            c[1] = int(c[1]/1)
+            pygame.draw.circle(screen, COLORS[j], c, 1, 1)
+            j += 1
         print(a)
+        print('i: ', i)
         pygame.display.flip()
     c = modele.routine(False)
 
--- truc.py
+++ truc.py
@@ -2,22 +2,31 @@ import math
 
 
 # CONSTANTES:
-G = 6.674e-11  # Constante de gravitation universelle
+G = 3.674e-2  # Constante de gravitation universelle
 M_SOLEIL = 1.989e30
 M_TERRE = 5.972e24
 M_LUNE = 7.342e22
-
+H = 6.0
 
 class Modele:
     def __init__(self):
         self.fps = 30
         self.corps = []
-        terre = Corps(x=384400e3, y=384400e3*2, masse=M_TERRE, vitesse=Vecteur(0, math.pi/2))
-        lune = Corps(x=384400e3*1.5, y=384400e3*2, masse=M_LUNE, vitesse=Vecteur(980, math.pi/2))
+        terre = Corps(x=300, y=400, masse=100000, vitesse=Vecteur(0.06, -math.pi/2))
+        lune = Corps(x=400, y=400, masse=1000, vitesse=Vecteur(6.06, math.pi/2))
+        new_horizon = Corps(x=350, y=486.6, masse=10, vitesse=Vecteur(6.06, 5*math.pi/6))
+        #new_horizon = Corps(x=350, y=313.14, masse=10, vitesse=Vecteur(6.06, -5*math.pi/6))
 
         lune.influences.append(terre)
-        terre.influences.append(lune)
+        lune.influences.append(new_horizon)
 
+        terre.influences.append(lune)
+        terre.influences.append(new_horizon)
+
+        new_horizon.influences.append(terre)
+        new_horizon.influences.append(lune)
+
+        self.corps.append(new_horizon)
         self.corps.append(lune)
         self.corps.append(terre)
 
@@ -25,6 +34,7 @@ class Modele:
         coords = []
         for corp in self.corps:
             corp.update()
+            #print(corp.position.y())
             if affichage:
                 coords.append([corp.position.x(), corp.position.y()])
                 #print('Coords:', coords)
@@ -62,6 +72,8 @@ class Vecteur:  # 2D
         return cart_to_pol(x, y)
 
     def __mul__(self, other):
+        if type(other) is float:
+            return Vecteur(self.norme*other, self.angle)
         x, y = self.norme*math.cos(self.angle), self.norme*math.sin(self.angle)
         x_p, y_p = other.norme*math.cos(other.angle), other.norme*math.sin(other.angle)
         return x*x_p+y*y_p, 10  # For 0 with 2 normal vectors
@@ -75,19 +87,24 @@ class Corps:
         self.influences = []
         self.masse = masse
         # Conds initiales
-        self.acceleration = Vecteur(0, 0)
         self.vitesse = vitesse
         self.position = cart_to_pol(x, y)
 
-    def update(self):
+    def acceleration(self):
+        acceleration = Vecteur(0, 0)
         for influence in self.influences:
             f1 = G*influence.masse / ((self.position.distance(influence.position)) ** 2) * self.masse
             f1 /= self.masse
             direction = self.position.angle_between(influence.position)
-            direction.norme = f1*1
-            self.acceleration = direction
-        self.vitesse += self.acceleration
-        self.position += self.vitesse
+            direction.norme = f1
+            acceleration += direction
+        return acceleration
+
+    def update(self):
+        acceleration0 = self.acceleration()
+        self.position += self.vitesse*H + acceleration0*((H**2)/2)
+        acceleration1 = self.acceleration()
+        self.vitesse += (acceleration0 + acceleration1)*(H/2)
 
     def self_to_other_vector(self, other):
         return self.position.angle_between(other.position)
