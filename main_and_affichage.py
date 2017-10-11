4935e79247b5464ce33ef5a67df0877aa5e681f4
Lambyor <lambyor@gmail.com>
2017-10-11 10:33:50 +0200

    Ajout de la branche cartésienne

--- main_and_affichage.py
+++ main_and_affichage.py
@@ -1,3 +1,5 @@
+from pycallgraph.output import GraphvizOutput
+from pycallgraph import PyCallGraph
 from mecanique_classique import *
 import pygame
 import random
@@ -11,60 +13,69 @@ DRAW = True  # Affichage des lignes
 HOMOTETIE = 1
 PAS = 1.5  # DE combien je me déplace
 DEPLACEMENT = 1  # Sera scalé avec l'homotétie. Ne sert à rien je crois
+MAX_I = 1000
 
 modele = Modele()
 REFERENT = modele.corps[0]
 COLORS = [tuple(random.randint(40, 255) for _ in range(3)) for i in range(len(modele.corps))]
 # COLORS = [(255, 255, 255), (0, 0, 0), (255, 0, 255)]
+def main():
+    global DONE
+    global DRAW
+    global HOMOTETIE
+    global DEPLACEMENT
+    i = 0
+    points = []
+    while not DONE and i < MAX_I:
+        if DRAW:
+            screen.fill((0, 0, 0))
 
-i = 0
-points = []
-while not DONE:
-    if DRAW:
-        screen.fill((0, 0, 0))
+    # ===========================================================================================
+    # NE SERT PAS A LA SIMULATION: GESTION DES TOUCHES. ON REGARDE PAS
+    # ===========================================================================================
+        for event in pygame.event.get():  # On parcourt la liste de tous les évènements reçus
+            if event.type == pygame.QUIT:  # Si un de ces évènements est de type QUIT
+                DONE = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    DRAW = not DRAW
+                    time.sleep(0.1)
 
-# ===========================================================================================
-# NE SERT PAS A LA SIMULATION: GESTION DES TOUCHES. ON REGARDE PAS
-# ===========================================================================================
-    for event in pygame.event.get():  # On parcourt la liste de tous les évènements reçus
-        if event.type == pygame.QUIT:  # Si un de ces évènements est de type QUIT
-            DONE = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                DRAW = not DRAW
-                time.sleep(0.1)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_z]:
+            modele.move(Vecteur(0, -DEPLACEMENT))
+        if keys[pygame.K_s]:
+            modele.move(Vecteur(0, DEPLACEMENT))
+        if keys[pygame.K_q]:
+            modele.move(Vecteur(-DEPLACEMENT, 0))
+        if keys[pygame.K_d]:
+            modele.move(Vecteur(DEPLACEMENT, 0))
+        if keys[pygame.K_i]:
+            HOMOTETIE /= 1.006
+        if keys[pygame.K_o]:
+            HOMOTETIE *= 1.006
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_z]:
-        modele.move(Vecteur(DEPLACEMENT, -math.pi/2))
-    if keys[pygame.K_s]:
-        modele.move(Vecteur(DEPLACEMENT, math.pi/2))
-    if keys[pygame.K_q]:
-        modele.move(Vecteur(DEPLACEMENT, math.pi))
-    if keys[pygame.K_d]:
-        modele.move(Vecteur(DEPLACEMENT, 0))
-    if keys[pygame.K_i]:
-        HOMOTETIE /= 1.006
-    if keys[pygame.K_o]:
-        HOMOTETIE *= 1.006
+        DEPLACEMENT = PAS * HOMOTETIE
 
-    DEPLACEMENT = PAS * HOMOTETIE
+    # ===========================================================================================
+    # FIN GESTION DES TOUCHES. FIN TRUCS APS INTERESSANTS
+    # ===========================================================================================
 
-# ===========================================================================================
-# FIN GESTION DES TOUCHES. FIN TRUCS APS INTERESSANTS
-# ===========================================================================================
+        i += 1
+        if i%100 is 0:
+            a = modele.routine(True)
+            j = 0
+            for c in a:
+                c[0] = int(c[0]/abs(HOMOTETIE))
+                c[1] = int(c[1]/abs(HOMOTETIE))
+                pygame.draw.circle(screen, COLORS[j], c, 1, 1)
+                j += 1
+            #print(a)
+            print('i: ', i)
+            pygame.display.flip()
+            time.sleep(0.0)
+        c = modele.routine(False)
 
-    i += 1
-    if i%1 is 0:
-        a = modele.routine(True)
-        j = 0
-        for c in a:
-            c[0] = int(c[0]/abs(HOMOTETIE))
-            c[1] = int(c[1]/abs(HOMOTETIE))
-            pygame.draw.circle(screen, COLORS[j], c, 1, 1)
-            j += 1
-        print(a)
-        print('i: ', i)
-        pygame.display.flip()
-        time.sleep(0.0)
-    c = modele.routine(False)
+if __name__ == '__main__':
+    with PyCallGraph(output=GraphvizOutput()):
+        main()
--- mecanique_classique.py
+++ mecanique_classique.py
@@ -1,30 +1,17 @@
+import random
 import math
-
 
 # CONSTANTES:
 G = 3.674e-2  # Constante de gravitation universelle
 M_SOLEIL = 1.989e30
 M_TERRE = 5.972e24
 M_LUNE = 7.342e22
-H = 0.2
+H = 0.1
 
 
 class Modele:
     def __init__(self):
-        self.fps = 30
-        self.corps = []
-        terre = Corps(x=300, y=400, masse=100000, vitesse=Vecteur(-0.00, math.pi/2))
-        terre2 = Corps(x=600, y=400, masse=100000, vitesse=Vecteur(3.00, -math.pi/2))
-        lune = Corps(x=400, y=400, masse=1000, vitesse=Vecteur(6.06, -math.pi/2))
-        new_horizon = Corps(x=350, y=486.6, masse=10, vitesse=Vecteur(6.06, 5*math.pi/6))
-        new_horizon2 = Corps(x=200, y=400, masse=10, vitesse=Vecteur(6.06, math.pi/2))
-        new_horizon = Corps(x=350, y=313.14, masse=10, vitesse=Vecteur(6.06, -5*math.pi/6))
-
-        self.corps.append(new_horizon)
-        self.corps.append(new_horizon2)
-        #self.corps.append(terre2)
-        self.corps.append(lune)
-        self.corps.append(terre)
+        self.corps = [Corps(x=random.randint(0, 500), y=random.randint(0, 500), masse=1000) for a in range(20)]
         self.liaisons()
 
     def liaisons(self):
@@ -39,7 +26,7 @@ class Modele:
             corp.update()
             #print(corp.position.y())
             if affichage:
-                coords.append([corp.position.x(), corp.position.y()])
+                coords.append([corp.position.x, corp.position.y])
                 #print('Coords:', coords)
         return coords
 
@@ -49,41 +36,37 @@ class Modele:
 
 
 class Vecteur:  # 2D
-    def __init__(self, norme, angle):  # TRES TRES SALE
-        self.norme = norme
-        self.angle = angle
-
-    def x(self):
-        return self.norme*math.cos(self.angle)
-
-    def y(self):
-        return self.norme*math.sin(self.angle)
-
-    def projection_cartesienne(self):
-        return self.x(), self.y()
+    def __init__(self, x, y):  # TRES TRES SALE
+        self.x = x
+        self.y = y
 
     def angle_between(self, other):
-        x = other.x() - self.x()
-        y = other.y() - self.y()
-        return cart_to_pol(x, y)  # Radians
+        x = other.x - self.x
+        y = other.y - self.y
+        a = x*y
+        b = math.sqrt(((other.x-self.x)**2)*((other.y-self.y)**2))
+        if b == 0:
+            b = 0.001
+        return a/b
 
     def distance(self, other):
-        return cart_to_pol(other.x() - self.x(), other.y() - self.y()).norme
+        return math.sqrt((other.x - self.x)**2+(other.y-self.y)**2)
+
+    def norme(self):
+        return math.sqrt(self.x**2+self.y**2)
 
     def __add__(self, other):
-        x = other.x() + self.x()
-        y = other.y() + self.y()
-        return cart_to_pol(x, y)
+        x = other.x + self.x
+        y = other.y + self.y
+        return Vecteur(x, y)
 
     def __mul__(self, other):
-        if type(other) is float:
-            return Vecteur(self.norme*other, self.angle)
-        x, y = self.norme*math.cos(self.angle), self.norme*math.sin(self.angle)
-        x_p, y_p = other.norme*math.cos(other.angle), other.norme*math.sin(other.angle)
-        return x*x_p+y*y_p, 10  # For 0 with 2 normal vectors
+        if type(other) is float:  # Mult by a scalar
+            return Vecteur(self.x*other, self.y*other)
+        return self.x*other.x+self.y*other.y  # For 0 with 2 normal vectors
 
     def __str__(self):  # PRINT IN DEGREES
-        return str((str(round(self.norme, 5)) + '.u', str(round(self.angle*180/math.pi, 5)) + '°'))
+        return str((str(round(self.x, 5)) + '.x', str(round(self.y, 5)) + '.y'))
 
 
 class Corps:
@@ -92,16 +75,20 @@ class Corps:
         self.masse = masse
         # Conds initiales
         self.vitesse = vitesse
-        self.position = cart_to_pol(x, y)
+        self.position = Vecteur(x, y)
 
     def acceleration(self):
         acceleration = Vecteur(0, 0)
         for influence in self.influences:
+            if self.position.distance(influence.position) <= 4:  # OVERFLOW ACCELERATION
+                return Vecteur(0, 0)
             f1 = G*influence.masse / ((self.position.distance(influence.position)) ** 2) * self.masse
             f1 /= self.masse
-            direction = self.position.angle_between(influence.position)
-            direction.norme = f1
-            acceleration += direction
+            angle = self.position.angle_between(influence.position)
+            x = f1 * (influence.position.x - self.position.x)
+            y = f1 * (influence.position.y - self.position.y)
+            #print('Accel:', Vecteur(x, y))
+            acceleration += Vecteur(x, y)
         return acceleration
 
     def update(self):
@@ -113,22 +100,10 @@ class Corps:
     def self_to_other_vector(self, other):
         return self.position.angle_between(other.position)
 
-
-def cart_to_pol(x, y):
-    if is_close(x, 0) and is_close(y, 0):
-        theta = 0   # C kom ca vect nul => 0° incl
-    elif x > 0 and y >= 0:
-        theta = math.atan(y / x)
-    elif x > 0 and y < 0:
-        theta = math.atan(y / x) + 2 * math.pi
-    elif x < 0:
-        theta = math.atan(y / x) + math.pi
-    elif x == 0 and y > 0:
-        theta = math.pi / 2
-    elif x == 0 and y < 0:
-        theta = 3 * math.pi / 2
-    norme = math.sqrt(x**2+y**2)
-    return Vecteur(norme, theta)
+    def merge(self, other):
+        self.masse += other.masse
+        self.vitesse += other.vitesse
+        other.position = Vecteur(10000, 0)
 
 
 def is_close(a, b, rel_tol=1e-15):
@@ -136,13 +111,15 @@ def is_close(a, b, rel_tol=1e-15):
     return dist < rel_tol
 
 if __name__ == "__main__":
-    terre = Corps(x=100, y=200, masse=M_TERRE)
-    satellite = Corps(x=100, y=0, masse=1, corps=terre)
+    terre = Corps(x=100, y=200, masse=1000)
+    satellite = Corps(x=100, y=0, masse=1)
+    satellite.influences.append(terre)
     print('Pos1:', satellite.position)
     print('Vit1', satellite.vitesse)
-    print('Accel1', satellite.acceleration)
     for a in range(10):
         satellite.update()
-    print('Pos2:', satellite.position.y())
+    print('Pos2:', satellite.position.y)
     print('Vit2', satellite.vitesse)
-    print('Accel2', satellite.acceleration)
+    a  = Vecteur(0.01, 0.01)
+    b = Vecteur(1, 1)
+    print(a.angle_between(b)*180/math.pi)
