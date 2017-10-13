e3f5142c9d5f2a4e5feac83af54ebe3c4e8acfeb
Lambyor <lambyor@gmail.com>
2017-10-13 13:51:00 +0200

    QuadTree vraiment propre

--- QT.java
+++ QT.java
@@ -1,64 +1,48 @@
 public class QT{
-    private static final int MAX_PONTS = 1;
-    private double[] p;  //Taille = 2
-    private double[] b;  // Taille = 4
-    private double[][] sB;
-    private QT[] f;  // Taille: 4
+    private double[] p;  // Taille = 2
+    private double[] b;  // Domaine de valeurs du noeud (Taille = 1 rectangle)
+    private double[][] sB;  // Sous domaines de valeurs reliés aux fils (Taille: 4 rectangles)
+    private QT[] f;  // fils du noeud actuel (Taille: 4 quadTrees)
     
     public double[] getP(){return this.p;}
     public void setP(double[] d){this.p = d;}
     
     public double[] getSB(int i){return this.sB[i];}
-    
-    public double[] getb(){return this.b;}
-    public void setb(double[] d){this.b = d;}
-    
-    public QT[] getF(){return this.f;}
+   
     public QT getFi(int i){return this.f[i];}
     public void setFi(int i, QT q){this.f[i] = q;}
     
-    public QT(double[] _p, double[] _b, QT[] _f){
+    private static boolean vide(QT q){ return q == null; }
+    
+    public QT(double[] _p, double[] _b){
+        QT[] QTVide = {null, null, null, null} ;
         this.p = _p;
         this.b = _b;
         this.sB = zones(this.b);
-        this.f = _f;
+        this.f = QTVide;  // Arbre vide au départ
     }
     
-    private static boolean vide(QT q){
-        return q == null;
-    }
-    
-    private static boolean feuille(QT qt){
-        return vide(qt.f[0]) && vide(qt.f[1]) && vide(qt.f[2]) && vide(qt.f[3]);
-    }
-    
-    public QT inserer(double[] point, double[] boundaries){
-        QT[] QTVide = {null, null, null, null} ;
+    public QT inserer(double[] point){
         for(int i=0; i<4; i++){
-            if(vide(this.f[i]) && pointInRect(point, this.sB[i])){
-                this.f[i] = new QT(point, this.sB[i], QTVide);
-            }
-            else if(pointInRect(point, this.sB[i])){
-                this.f[i].inserer(point, this.sB[i]);
-            }
+            if(vide(this.getFi(i)) && pointInRect(point, this.getSB(i)))
+                this.setFi(i, new QT(point, this.getSB(i)));
+            else if(pointInRect(point, this.getSB(i)))
+                this.getFi(i).inserer(point);
             if(this.p != null){
-                if(pointInRect(this.p, this.sB[i])){
-                    double[] point2 = this.p;                
-                    this.p = null;  //Sinon boucle infinie car inserer verrait p!=null
-                    this.inserer(point2, this.sB[i]);
+                if(pointInRect(this.getP(), this.getSB(i))){
+                    double[] pointTampon = this.getP();                
+                    this.setP(null);  // Sinon boucle infinie car inserer verrait p!=null
+                    this.inserer(pointTampon);
                 }
             }
         }
         return this;
     }    
-    
-    public static double[][] zones(double[] r){
-        //p: x,y
-        //r: x,y,x',y
-        
+
+    private static double[][] zones(double[] r){
         // Zones (Arbitraire):
-        //|0, 1|
-        //|2, 3|
+        // |0, 1|
+        // |2, 3|
         double dX = (r[0] + r[2])/2;
         double dY = (r[1] + r[3])/2;
         double[] z0 = {r[0], dY, dX, r[3]};
@@ -69,18 +53,20 @@ public class QT{
         return zones;
     }
     
-    public static boolean pointInRect(double[] p, double[] r){
-        //[x; x'[ et [y; y'[
+    private static boolean pointInRect(double[] p, double[] r){
+        // [x; x'[ et [y; y'[
         return r[0]<=p[0] && p[0]<r[2] && r[1]<=p[1] && p[1]<r[3];
     }
-
+    //+----------------------+
+    //|INUTILE A PARTIR D'ICI|
+    //+----------------------+
     public String toString(){
         String res = "|QuadTree:\n";
         if(this.p == null)
             res += "   |null\n";
-        else{
+        else
             res += "   |Pt:   (" + this.p[0] + ", " + this.p[1] + ")\n";
-        }
+        
         res += "   |Zone: (" + this.b[0] + ", " + this.b[1] + "; " +this.b[2] + ", "
             + this.b[3] + ")\n";
         
@@ -88,20 +74,17 @@ public class QT{
             + !vide(this.f[3]) + ")";
         return res;
     }
-    
-    public static void main(){
-        QT[] QTNull = {null, null, null, null};
 
+    public static void main(){
         double[] p1 = {1, 1};
-        double[] p2 = {2, 2};
+        double[] p2 = {1.1, 1.1};
         double[] p3 = {1, 2};
         double[] zone = {0, 0, 5, 5};
-        QT qt = new QT(p1, zone, QTNull);
+        QT qt = new QT(p1, zone);
         
-        qt.inserer(p2, zone);
-        //qt.inserer(p3, zone, qt);
-        System.out.println("1:\n" + qt.f[2].f[1]);
-        //qt.inserer(p3, zone, qt);
+        qt.inserer(p2);
+        qt.inserer(p3);
         
+        System.out.println(qt);
     }
 }
