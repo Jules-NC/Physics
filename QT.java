e13fdc07cd948afda65a3995683271763d3f6902
Lambyor <lambyor@gmail.com>
2017-10-13 19:55:28 +0200

    Ca remarche

--- QT.java
+++ QT.java
@@ -1,11 +1,11 @@
 public class QT{
-    private double[] p;  // Coordonées représentatives du point [x, y]
-    private double[] b;  // Domaine de valeurs du noeud, représenté par un rectangle [x, x', y, y']
-    private double[][] sB;  // Sous-domaines de valeurs reliés aux fils [rect0, rect1, rect2, rect3]
-    private QT[] f;  // fils du noeud actuel [QT0, QT1, QT2, QT3]
+    private double[] p;  // Coordonées représentatives du point (Taille = 1 point)
+    private double[] b;  // Domaine de valeurs du noeud (Taille = 1 rectangle)
     private double[] centreMasse ; // Centre de masse du noeud (Point x, y)
     private double masse; // Masse totale
-
+    private double[][] sB;  // Sous domaines de valeurs reliés aux fils (Taille: 4 rectangles)
+    private QT[] f;  // fils du noeud actuel (Taille: 4 quadTrees)
+    
     public double[] getP(){return this.p;}
     public void setP(double[] d){this.p = d;} 
     public double[] getSB(int i){return this.sB[i];}
@@ -15,41 +15,22 @@ public class QT{
     public void addMasse(double d){this.masse += d;}
     public double[] getCentreMasse(){return this.centreMasse;}
 
-    public QT(double[] _p, double[] _b){
+        
+    public QT(double[] _p, double[] _b, double _masse){
         QT[] QTVide = {null, null, null, null} ;
         this.p = _p;
         this.b = _b;
-        this.sB = zones(this.b);  // Création des sous-domaines de valeurs de la feuille.
-        this.f = QTVide;  // Ce QT est une feuille.
+        this.sB = zones(this.b);
+        this.f = QTVide;  // Arbre vide au départ
+        
+        this.centreMasse = null;
+        this.masse = _masse;
     }
-
-    private static boolean vide(QT q){return q == null;}
     
+    private static boolean vide(QT q){ return q == null; }
+
     private static boolean feuille(QT q){ // Boucle for si besoin de généraliser à un n-tree
         return vide(q.getFi(0)) && vide(q.getFi(1)) && vide(q.getFi(2)) && vide(q.getFi(3));
-    }
-
-    public QT inserer(double[] point){
-        for(int i=0; i<4; i++){  // On itère sur les 4 sous emplacements.
-            // Si le point a pour destination un sous-arbre vide, on crée cet arbre.
-            if(vide(this.getFi(i)) && pointInRect(point, this.getSB(i))) 
-                this.setFi(i, new QT(point, this.getSB(i)));
-            // Si le point a pour destination un sous arbre non vide, on insère le point dans ce sous-arbre.
-            else if(pointInRect(point, this.getSB(i)))
-                this.getFi(i).inserer(point);
-            // Si cet arbre contient une valeur => ETAIT une feuille, on insère cette valeur dans le sous-arbre correspondant, 
-            // puis on supprime cette valeur de l'arbre. Cet arbre devient ainsi un noeud.
-            if(this.p != null){  // (INCERTAIN) Sert à éviter un nullPointerException dans pointInRect.
-                if(pointInRect(this.getP(), this.getSB(i))){
-                    double[] pointTampon = this.getP();                
-                    this.setP(null);  // Sinon boucle infinie car inserer verrait p non null.
-                    this.inserer(pointTampon);
-                }
-            }
-        }
-        // La valeur de l'arbre n'est pas forcément située dans le même sous domaine que celui du point. On doit donc parcourir
-        // les 4 sous-domaines, et donc retourner l'arbre ici.
-        return this;
     }
     
     private double[] updateCentreMasse(){
@@ -74,8 +55,24 @@ public class QT{
         this.centreMasse = res;
         return res;
     }
+    
+    public QT inserer(double[] point, double masse){
+        for(int i=0; i<4; i++){
+            if(vide(this.getFi(i)) && pointInRect(point, this.getSB(i)))
+                this.setFi(i, new QT(point, this.getSB(i), masse));
+            else if(pointInRect(point, this.getSB(i)))
+                this.getFi(i).inserer(point, masse);
+            if(this.p != null){
+                if(pointInRect(this.getP(), this.getSB(i))){
+                    double[] pointTampon = this.getP();                
+                    this.setP(null);  // Sinon boucle infinie car inserer verrait p!=null
+                    this.inserer(pointTampon, masse);
+                }
+            }
+        }
+        return this;
+    }    
 
-    // Renvoie les 4 sous rectangles d'un rectangle (Séparation au milieu).
     private static double[][] zones(double[] r){
         // Zones (Arbitraire):
         // |0, 1|
@@ -89,18 +86,17 @@ public class QT{
         double[][] zones = {z0, z1, z2, z3};
         return zones;
     }
-
+    
     private static boolean pointInRect(double[] p, double[] r){
-        // Choix arbitraire d'inclusion/exclusion
-        // [x; x'[ et [y; y'[ sinon un point pourrait être présent dans plus d'un rectangle
+        // [x; x'[ et [y; y'[
         return r[0]<=p[0] && p[0]<r[2] && r[1]<=p[1] && p[1]<r[3];
-}
+    }
     //+----------------------+
     //|INUTILE A PARTIR D'ICI|
     //+----------------------+
     public String toString(){
         String res = "|QuadTree:\n";
-        if(this.p == null)
+        if(this.p == null)  
             res += "   |null\n";
         else
             res += "   |Pt:   (" + this.p[0] + ", " + this.p[1] + ")\n";
@@ -114,15 +110,15 @@ public class QT{
     }
 
     public static void main(){
-        double[] p1 = {1, 1};
-        double[] p2 = {1.1, 1.1};
-        double[] p3 = {1, 2};
-        double[] zone = {0, 0, 5, 5};
-        QT qt = new QT(p1, zone);
+        double[] p1 = {0, 0};
+        double[] p2 = {0, 4};
+        double[] p3 = {0, 8};
+        double[] zone = {0, 0, 16, 16};
+        QT qt = new QT(p1, zone, 10);
         
-        qt.inserer(p2);
-        qt.inserer(p3);
-        
+        qt.inserer(p2, 10);
+        qt.inserer(p3, 10);
+        qt.updateCentreMasse();
         System.out.println(qt);
     }
 }
